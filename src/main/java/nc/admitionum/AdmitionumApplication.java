7475a56ad458b9fb43893d3193221c9cb3ad5b77
Franer <franernarf@gmail.com>
2026-07-25 12:45:17 +0200

    chore: initialize Admitionum Spring Boot project.

---
+++ src/main/java/nc/admitionum/AdmitionumApplication.java
@@ -0,0 +1,13 @@
+package nc.admitionum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmitionumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmitionumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nc/admitionum/AdmitionumApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.admitionum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmitionumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
